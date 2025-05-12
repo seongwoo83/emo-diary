650294f01e289a3198387b38e296b74264fe855c
jsw <jeongseongwoo83@daum.net>
2025-05-12 17:11:23 +0900

    [feat] route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import './App.css'
-
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Diary from './pages/diary/Diary'
+import New from './pages/new/New'
+import NotFound from './pages/404/NotFound'
+/**
+  1. "/": 모든 일기를 조회하는 Home페이지
+  2. "/new": 새로운 일기를 작성하는 New페이지
+  3. "/diary": 일기를 상세히 조회하는 Diary페이지
+*/
 function App() {
 
   return (
-    <>
-    </>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/new' element={<New />} />
+      <Route path='/diary' element={<Diary />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   )
 }
 
